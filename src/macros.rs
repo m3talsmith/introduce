7aae17e42156a8cd7970948be01c9f9fa8368071
Michael Christenson II <m3talsmith@gmail.com>
2025-08-07 20:01:04 -0400

    remove the uneccasary title casing

--- src/macros.rs
+++ src/macros.rs
@@ -5,18 +5,15 @@ macro_rules! introduce {
     };
     ($name:expr) => {
         let name: &str = $name;
-        let name = name[0..1].to_uppercase() + &name[1..];
         println!("Hello, I'm {}!", name);
     };
     ($name:expr, $age:expr) => {
         let name: &str = $name;
-        let name = name[0..1].to_uppercase() + &name[1..];
         let age: i32 = $age;
         println!("Hello, I'm {} and I'm {} years old!", name, age);
     };
     ($name:expr, $age:expr, $($items:tt)*) => {
         let name: &str = $name;
-        let name = name[0..1].to_uppercase() + &name[1..];
         let age: i32 = $age;
         let items: Vec<&str> = $($items)*;
         let mut message = format!("Hello, I'm {} and I'm {} years old!\n\nI have {} items in my backpack:\n", name, age, items.len());
